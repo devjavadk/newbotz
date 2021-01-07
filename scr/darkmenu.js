7e3ef56e9427403544928844bffc1eb51aa01f28
devjavadk <76820782+devjavadk@users.noreply.github.com>
2021-01-07 17:27:56 -0300

    Add files via upload

---
+++ scr/darkmenu.js
@@ -0,0 +1,39 @@
+const devmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Dev:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  GABRIEL BOTZ👇
+  WA.me/5511946817667
+╚════════════════════`
+}
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
